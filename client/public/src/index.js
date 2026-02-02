7dc50a1a184a9636bd6b202e063acf88f3875de8
faizyabiqbal <faizyabiqbal21@gmail.com>
2026-02-02 12:21:08 +0500

    Force add client/src/index.js

--- client/public/src/index.js
+++ client/public/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // This imports your main styles
-import App from './App'; // This imports your main App component
+import './index.css';
+import App from './App';
 
-// This finds the <div> inside index.html and puts your App inside it
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

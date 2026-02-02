92bc9533a53f80a5fae27167b07ab99d9b8b3c04
faizyabiqbal <faizyabiqbal21@gmail.com>
2026-02-02 12:16:22 +0500

    Add missing index.js file

--- client/public/src/index.js
+++ client/public/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // This imports your main styles
+import App from './App'; // This imports your main App component
 
+// This finds the <div> inside index.html and puts your App inside it
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
